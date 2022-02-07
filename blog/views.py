873fba3c3316ba11271b10a877b7e9bcaec35d41
Maria Isadora <maria.isa03@outlook.com>
2022-02-07 16:38:23 -0300

    My Django Girls app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+#importing to show the real posts thats were published
+from django.utils import timezone
+#display this model
+from .models import Post
 
-# Create your views here.
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def post_list(request): 
+    #shows actual publications sorted by publication date
+    #'post' is a variable to QuerySet
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
